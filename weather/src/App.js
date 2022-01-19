967f05e820c24dfdc0d204c59da270abbd8e5066
Lidor Ohana <lidorrohana@gmail.com>
2022-01-19 22:47:32 +0200

    main

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,52 @@
-
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import {BrowserRouter as Router,Routes,Route, Link, Navigate} from 'react-router-dom'
+import Weather from './components/Weather';
+import Favorites from './components/Favorites';
+import Header from './components/Header';
+import Test from './components/Test';
 
 function App() {
+  const [yourFavorites, setyourFavorites] = useState("")
+  const [height, setheight] = useState(window.innerHeight);
+  const [width, setwidth] = useState(window.innerWidth);
+  const [inputSearch, setInputSearch] = useState("");
+  const [city, setCity] = useState("Tel Aviv");
+  const [citiesList , setCitiesList]= useState("")
+
+  useEffect(() => {
+    function handleResize() {
+        setheight(window.innerHeight);
+        setwidth(window.innerWidth);  
+    }
+    window.addEventListener('resize', handleResize);
+},[])
+useEffect(()=>{
+  console.log("get date frm server");
+  setCity(inputSearch);
+},[inputSearch])
+
+// useEffect(()=>{
+//   fetch('http://dataservice.accuweather.com/locations/v1/cities/autocomplete')
+//   .then((data)=>{
+//     console.log(data);
+//     setCitiesList(data)})
+  
+// },[])
+
+
   return (
-    <div className="App">
-      <h1>Weather</h1>
+    // className="App"
+    <div style={{width:`${width}px`,height:`${height}px`}} >
+        <Router>
+          <Header/>
+    <Routes>
+      <Route path='/' element={<Weather onUpdate={(input)=>{ setInputSearch(input)}}/>}/>
+      <Route path='/test' element={<Test/>}/>
+      <Route path='/favorites' element={<Favorites/>}/>
+
+    </Routes>
+     </Router>
     </div>
   );
 }
